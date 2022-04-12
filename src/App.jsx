9b1532d2f86dfc473680fdda74e636b6a505d2f1
denisetyawan <denisetyawanseven@gmail.com>
2022-04-12 23:11:06 +0700

    Homework module 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
-import './App.css';
-import Home from './pages/Home';
+import CreatePlaylist from './pages/CreatePlaylist';
+import { useLocation, Switch, Route } from 'react-router-dom';
+import Auth from './pages/Auth';
+import GuardRoute from './components/GuardRoute';
+import NotFound from './pages/NotFound';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout } from './slice/authSlice';
 
 function App() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const accessTokenState = useSelector((state) => state.auth.accessToken);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+      const expiredDate = localStorage.getItem('expiredDate');
+
+      if (expiredDate < +new Date()) {
+        dispatch(logout());
+      } else if (!accessTokenState) {
+        const user = JSON.parse(localStorage.getItem('user'));
+        dispatch(login({
+          accessToken,
+          user,
+          expiredDate,
+        }));
+      }
+    }
+  }, [accessTokenState, dispatch, location.pathname]);
+
   return (
-    <Home />
+    <Switch>
+      <GuardRoute path="/create-playlist" type="private" exact>
+        <CreatePlaylist />
+      </GuardRoute>
+      <GuardRoute path="/" type="guest" exact>
+        <Auth />
+      </GuardRoute>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
   );
 }
 
